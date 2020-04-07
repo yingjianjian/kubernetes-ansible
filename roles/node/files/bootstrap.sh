2d2593e0490956fe0ae3c7da9f28b7766398a71b
root <429795337@qq.com>
2020-04-07 13:20:24 +0800

    add kubelet

---
+++ roles/node/files/bootstrap.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+TOKEN_PUB=$(openssl rand -hex 3)
+TOKEN_SECRET=$(openssl rand -hex 8)
+BOOTSTRAP_TOKEN="${TOKEN_PUB}.${TOKEN_SECRET}"
+
+kubectl -n kube-system create secret generic bootstrap-token-${TOKEN_PUB} \
+        --type 'bootstrap.kubernetes.io/token' \
+        --from-literal description="cluster bootstrap token" \
+        --from-literal token-id=${TOKEN_PUB} \
+        --from-literal token-secret=${TOKEN_SECRET} \
+        --from-literal usage-bootstrap-authentication=true \
+        --from-literal usage-bootstrap-signing=true
+
+
+CLUSTER_NAME="kubernetes"
+KUBE_USER="kubelet-bootstrap"
+KUBE_CONFIG="bootstrap.kubeconfig"
+
+# 设置集群参数
+kubectl config set-cluster ${CLUSTER_NAME} \
+  --certificate-authority=/etc/kubernetes/pki/ca.crt \
+  --embed-certs=true \
+  --server=https://10.30.30.38:8443\
+  --kubeconfig=/etc/kubernetes/${KUBE_CONFIG}
+
+# 设置上下文参数
+kubectl config set-context ${KUBE_USER}@${CLUSTER_NAME} \
+  --cluster=${CLUSTER_NAME} \
+  --user=${KUBE_USER} \
+  --kubeconfig=/etc/kubernetes/${KUBE_CONFIG}
+
+# 设置客户端认证参数
+kubectl config set-credentials ${KUBE_USER} \
+  --token=${BOOTSTRAP_TOKEN} \
+  --kubeconfig=/etc/kubernetes/${KUBE_CONFIG}
+
+# 设置当前使用的上下文
+kubectl config use-context ${KUBE_USER}@${CLUSTER_NAME} --kubeconfig=/etc/kubernetes/${KUBE_CONFIG}
+
+# 查看生成的配置文件
+kubectl config view --kubeconfig=/etc/kubernetes/${KUBE_CONFIG}
+
+if [[ `kubectl get clusterrolebinding | grep  kubeadm:kubelet-bootstrap` == '' ]];then
+kubectl create clusterrolebinding kubeadm:kubelet-bootstrap \
+        --clusterrole system:node-bootstrapper --group system:bootstrappers
+fi
+
+cat <<EOF | kubectl apply -f -
+# Approve all CSRs for the group "system:bootstrappers"
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: auto-approve-csrs-for-group
+subjects:
+- kind: Group
+  name: system:bootstrappers
+  apiGroup: rbac.authorization.k8s.io
+roleRef:
+  kind: ClusterRole
+  name: system:certificates.k8s.io:certificatesigningrequests:nodeclient
+  apiGroup: rbac.authorization.k8s.io
+EOF
+cat <<EOF | kubectl apply -f -
+# Approve renewal CSRs for the group "system:nodes"
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: auto-approve-renewals-for-nodes
+subjects:
+- kind: Group
+  name: system:nodes
+  apiGroup: rbac.authorization.k8s.io
+roleRef:
+  kind: ClusterRole
+  name: system:certificates.k8s.io:certificatesigningrequests:selfnodeclient
+  apiGroup: rbac.authorization.k8s.io
+EOF
+if [[ `kubectl get serviceaccount | grep  mastervip` == '' ]];then
+  kubectl create serviceaccount mastervip
+fi
+
+echo ${BOOTSTRAP_TOKEN},kubelet-bootstrap,10001,"system:bootstrappers" > /etc/kubernetes/pki/token.csv
+if [[ `kubectl get clusterrolebinding | grep  master-admin` == '' ]];then
+  kubectl create clusterrolebinding master-admin --clusterrole=cluster-admin --user=mastervip
+fi
+
+
